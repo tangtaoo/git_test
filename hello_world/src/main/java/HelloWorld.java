24deb5cc3738c173e55a1a1dab7a7f390e6bb510
tang <2386291623@qq.com>
2025-02-10 16:15:34 +0800

    create HelloWorld.java

---
+++ hello_world/src/main/java/HelloWorld.java
@@ -0,0 +1,11 @@
+/*
+  Description: 将测试程序上传到github上
+  Date: 2025/2/10 
+  Time: 16:14
+  Author: TangTao
+*/
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
